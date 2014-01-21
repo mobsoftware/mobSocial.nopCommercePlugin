f06b3ff759b3e456fa915dc7f0897b8b3c9e884f
breakskater <breakskater@gmail.com>
2014-01-20 22:51:02 -0600

    Upload Album Picture Feature

---
+++ Nop.Plugin.Widgets.mobSocial/Core/ICustomerAlbumPictureService.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Nop.Core.Domain.Catalog;
+using Nop.Plugin.Widgets.MobSocial.Domain;
+using Nop.Plugin.Widgets.MobSocial.Controllers;
+
+namespace Nop.Plugin.Widgets.MobSocial.Core
+{
+    /// <summary>
+    /// Product service
+    /// </summary>
+    public interface ICustomerAlbumPictureService
+    {
+
+        byte[] CreateThumbnailPicture(byte[] pictureBinary, int maxWidth, string mimeType);
+        CustomerAlbum GetCustomerAlbum(int customerId);
+        CustomerAlbum GetCustomerAlbumById(int albumId);
+
+        void Insert(CustomerAlbumPicture customerAlbumPicture);
+        void Delete(CustomerAlbumPicture customerAlbumPicture);
+
+        CustomerAlbumPicture GetCustomerAlbumPictureById(int customerAlbumPictureId);
+
+    }
+
+}
---
+++ Nop.Plugin.Widgets.mobSocial/Themes/mobSocial/Content/mobSocial/customer-albums.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="common.js" />
+
+
+$(document).ready(function () {
+
+
+    $("div.add-picture-button").click(function() {
+        alert('clicked add picture button!!');
+    });
+
+    
+});
