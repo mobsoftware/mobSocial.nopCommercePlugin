7d754247e775b34042937cd712ac3a4c96b17da3
breakskater <breakskater@gmail.com>
2014-07-18 10:13:21 -0500

    Implemented ability to edit Event Page Info

--- Nop.Plugin.Widgets.mobSocial/Controllers/Admin/ManageEventPageController.cs
+++ Nop.Plugin.Widgets.mobSocial/Controllers/Admin/ManageEventPageController.cs
@@ -7,6 +7,13 @@ using Nop.Plugin.Widgets.MobSocial.Core;
 using System.Collections.Generic;
 using System.Linq;
 using Nop.Web.Framework.Controllers;
+using Nop.Plugin.Widgets.MobSocial.Models;
+using Nop.Plugin.Widgets.MobSocial.Domain;
+using Nop.Core;
+using Nop.Services.Seo;
+using Nop.Services.Localization;
+using Nop.Services.Logging;
+using System;
 
 namespace Nop.Plugin.Widgets.MobSocial.Admin.Controllers
 {
@@ -16,14 +23,27 @@ namespace Nop.Plugin.Widgets.MobSocial.Admin.Controllers
         #region Fields
         private readonly IEventPageService _eventPageService;
         private readonly IPermissionService _permissionService;
+        private readonly IWorkContext _workContext;
+        private readonly IUrlRecordService _urlRecordService;
+        private readonly ILocalizationService _localizationService;
+        private readonly ICustomerActivityService _customerActivityService;
         #endregion
 
         #region Constructors
 
-        public ManageEventPageController(IPermissionService permissionService, IEventPageService eventPageService)
+        public ManageEventPageController(IPermissionService permissionService, 
+            IEventPageService eventPageService,
+            IWorkContext workContext,
+            IUrlRecordService urlRecordService,
+            ILocalizationService localizationService,
+            ICustomerActivityService customerActivityService)
         {
             _permissionService = permissionService;
             _eventPageService = eventPageService;
+            _workContext = workContext;
+            _urlRecordService = urlRecordService;
+            _localizationService = localizationService;
+            _customerActivityService = customerActivityService;
         }
 
         #endregion
@@ -56,6 +76,8 @@ namespace Nop.Plugin.Widgets.MobSocial.Admin.Controllers
             foreach(var item in items)
             {
                 var model = new {
+                    Id = item.Id,
+                    PictureThumbnailUrl = "",
                     FriendlyName = item.Name,
                     SystemName = item.Name,
                     IsPrimaryTaxProvider = false
@@ -79,6 +101,116 @@ namespace Nop.Plugin.Widgets.MobSocial.Admin.Controllers
         {
             return View("Index");
         }
+
+        [AdminAuthorize]
+        public ActionResult Edit(int id)
+        {
+            //todo: add later
+            //if (!_permissionService.Authorize(MobSocialPermissionProvider.ManageEventPages))
+            //    return AccessDeniedView();
+
+            var item = _eventPageService.GetById(id);
+
+            if (item == null) //Not found
+                return RedirectToAction("List");
+
+
+            var model = new EventPageModel()
+            {
+                Id = item.Id,
+                Name = item.Name,
+                SeName = item.GetSeName(0),
+                Address1 = item.Address1,
+                Address2 = item.Address2,
+                ZipPostalCode = item.ZipPostalCode,
+                DateCreated = item.DateCreated,
+                DateUpdated = item.DateUpdated,
+            };
+
+            return View("Edit", model);
+
+        }
+
+
+
+
+        [HttpPost, ParameterBasedOnFormName("save-continue", "continueEditing")]
+        public ActionResult Edit(EventPageModel model, bool continueEditing)
+        {
+            //todo: add later
+            //if (!_permissionService.Authorize(MobSocialPermissionProvider.ManageEventPages))
+            //    return AccessDeniedView();
+
+            var item = _eventPageService.GetById(model.Id);
+            if (item == null)
+                //No product found with the specified id
+                return RedirectToAction("List");
+
+            if (ModelState.IsValid)
+            {
+
+
+                item.Name = model.Name;
+                item.Address1 = model.Address1;
+                item.Address2 = model.Address2;
+                item.ZipPostalCode = model.ZipPostalCode;
+                item.DateUpdated = DateTime.Now;
+
+                _eventPageService.Update(item);
+
+                //search engine name
+                model.SeName = item.GetSeName(_workContext.WorkingLanguage.Id);
+
+                _urlRecordService.SaveSlug(item, model.SeName, _workContext.WorkingLanguage.Id);
+
+                //picture seo names
+                //UpdatePictureSeoNames(product);
+
+                SuccessNotification(_localizationService.GetResource("Admin.EventPage.Updated"));
+
+                if (continueEditing)
+                {
+                    //selected tab
+                    SaveSelectedTabIndex();
+
+                    return RedirectToAction("Edit", new { id = item.Id });
+                }
+                else
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View(model);
+        }
+
+        //delete product
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            //todo: add later
+            //if (!_permissionService.Authorize(MobSocialPermissionProvider.ManageEventPages))
+            //    return AccessDeniedView();
+
+
+            var item = _eventPageService.GetById(id);
+            if (item == null)
+                return RedirectToAction("List");
+
+        
+            _eventPageService.Delete(item);
+
+            //activity log
+            _customerActivityService.InsertActivity("DeleteEventPage", _localizationService.GetResource("ActivityLog.DeleteEventPage"), item.Name);
+
+            SuccessNotification(_localizationService.GetResource("Admin.EventPage.Deleted"));
+            return RedirectToAction("Index");
+        }
+
+
+
+
+
        
         #endregion
 
---
+++ Nop.Plugin.Widgets.mobSocial/Models/EventPageModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Nop.Web.Framework;
+using Nop.Web.Framework.Mvc;
+using System;
+
+namespace Nop.Plugin.Widgets.MobSocial.Models
+{
+    public class EventPageModel : BaseNopEntityModel
+    {
+        public string Name { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string ZipPostalCode { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+
+
+        public string SeName { get; set; }
+    }
+}
