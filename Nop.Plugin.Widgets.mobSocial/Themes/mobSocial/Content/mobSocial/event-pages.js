36bc556e5f236b7ef86580217b2dc1aba3dfef26
breakskater <breakskater@gmail.com>
2014-08-01 17:06:02 -0500

    MobSocial Core - added a couple useful angular directives to the core javascript file

--- Nop.Plugin.Widgets.mobSocial/Themes/mobSocial/Content/mobSocial/event-pages.js
+++ Nop.Plugin.Widgets.mobSocial/Themes/mobSocial/Content/mobSocial/event-pages.js
@@ -199,17 +199,6 @@ app.controller('EventPageButtonsController', ['$rootScope', '$scope', '$http', '
 
 
 
-app.directive('whenScrolled', function () {
-    return function (scope, elm, attr) {
-        var raw = elm[0];
-
-        elm.bind('scroll', function () {
-            if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
-                scope.$apply(attr.whenScrolled);
-            }
-        });
-    };
-});
 
 
 var AttendanceStatusEnum =  {
--- Nop.Plugin.Widgets.mobSocial/Themes/mobSocial/Content/mobSocial/mobSocial.js
+++ Nop.Plugin.Widgets.mobSocial/Themes/mobSocial/Content/mobSocial/mobSocial.js
@@ -1,2 +1,34 @@
 ﻿var app = angular.module('mobSocialApp', []);
 
+
+
+/* Angular Directives */
+
+app.directive('whenScrolled', function () {
+    return function (scope, elm, attr) {
+        var raw = elm[0];
+
+        elm.bind('scroll', function () {
+            if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
+                scope.$apply(attr.whenScrolled);
+            }
+        });
+    };
+});
+
+
+app.directive('openDialog', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, elem, attr, ctrl) {
+            var dialogId = '#' + attr.openDialog;
+            elem.bind('click', function (e) {
+                $(dialogId).dialog().dialog('open');
+            });
+        }
+    };
+});
+
+
+/* End Angular Directives */
+
