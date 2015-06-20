34b113e60d181ac239599008c974e7534da05b16
breakskater <breakskater@gmail.com>
2015-06-20 15:30:19 -0500

    Profile Pages: Favorite Songs Feature

---
+++ Nop.Plugin.Widgets.mobSocial/Core/FeatureToggle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Plugin.Widgets.MobSocial.Core
+{
+    /// <summary>
+    /// For disabling partially implmented features on the main branch.
+    /// Remove properties and associated if statements when feature is complete, or 
+    /// make features configurable bassed on user roles
+    /// </summary>
+    /// <see cref="http://martinfowler.com/bliki/FeatureToggle.html"/>
+    public static class FeatureToggle
+    {
+        public static bool MobLiveStreamEnabled { get { return false; } }
+        public static bool FavoriteSongEnabled { get { return false; } }
+        public static bool TimeLinePostsEnabled { get { return false; } }
+    }
+}
