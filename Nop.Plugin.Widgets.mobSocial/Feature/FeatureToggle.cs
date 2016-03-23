07bf2047aa6c33c6e7e33a02dafb3435b885773c
anshul <anshul@apexol.com>
2016-03-23 21:33:14 +0530

    Implemented MobSocialDefault theme as child theme to nopCommerce DefaultCleanTheme, implemented as SCSS/SASS. Removed unnecessary files.

---
+++ Nop.Plugin.Widgets.mobSocial/Feature/FeatureToggle.cs
@@ -0,0 +1,26 @@
+﻿namespace Nop.Plugin.Widgets.MobSocial.Feature
+{
+    public class FeatureToggle
+    {
+        public static bool FavoriteSongEnabled
+        {
+            get { return false; }
+        }
+
+        public static bool TimeLinePostsEnabled
+        {
+            get { return true; }
+        }
+
+        public static bool MobLiveStreamEnabled
+        {
+            get { return false; }
+        }
+
+        public static bool VideoBattlesEnabled
+        {
+            get { return true; }
+        }
+
+    }
+}
