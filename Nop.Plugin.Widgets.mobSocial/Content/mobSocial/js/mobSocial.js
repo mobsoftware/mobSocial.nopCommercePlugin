28dd9891592aaaa8463770e858916559dc1a1519
anshul <anshul@apexol.com>
2016-03-25 20:07:21 +0530

    created a global login method for easy redirection to login page for pages where authentication is required

--- Nop.Plugin.Widgets.mobSocial/Content/mobSocial/js/mobSocial.js
+++ Nop.Plugin.Widgets.mobSocial/Content/mobSocial/js/mobSocial.js
@@ -2,3 +2,12 @@
     .constant('rootUrl', './Plugins/Widgets.mobSocial/Content/mobSocial')
     .constant('globalApiEndPoint', '/api');
     
+//attach some global functions to rootScope
+app.run(function ($rootScope) {
+    $rootScope.login = function (returnUrl) {
+        //because the returnUrl may be absolute, it's better to explicitly reference the path from url for proper functioning
+        var a = document.createElement("a");
+        a.href = returnUrl;
+        window.location.href = "/login?ReturnUrl=" + encodeURIComponent(a.pathname);
+    };
+});
