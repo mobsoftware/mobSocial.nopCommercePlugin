46bfbafe18711cc6543a706daebaa9a71f7813bb
anshul <anshul@apexol.com>
2016-09-30 19:44:58 +0530

    client scripts for skills added

---
+++ Nop.Plugin.Widgets.mobSocial/Content/mobSocial/js/controllers/skills/skill-controller.js
@@ -0,0 +1,47 @@
+﻿app.controller("SkillController",
+[
+    "$scope", "$sce", "SkillService", function($scope, $sce, SkillService) {
+
+        $scope.getUserSkills = function(userId) {
+            SkillService.getUserSkills(userId,
+                function(response) {
+                    if (response.Success) {
+                        $scope.userSkills = response.UserSkills;
+                    }
+                });
+        }
+
+        $scope.getSkill = function(id) {
+            SkillService.getSkill(id,
+               function (response) {
+                   if (response.Success) {
+                       $scope.skill = response.UserSkill;
+                   }
+               });
+        }
+
+        $scope.postSkill = function (id) {
+            SkillService.postSkill(id,
+               function (response) {
+                   if (response.Success) {
+                       $scope.skill = response.UserSkill;
+                   }
+               });
+        }
+
+        $scope.delete = function (id) {
+            SkillService.delete(id,
+               function (response) {
+                   if (response.Success) {
+                       if ($scope.userSkills && $scope.skill) {
+                           for (var i = 0; i < $scope.userSkills.length; i++) {
+                               if ($scope.userSkills[i].Id == $scope.skill.Id) {
+                                   $scope.userSkills.slice(i, 1);
+                               }
+                           }
+                       }
+                   }
+               });
+        }
+    }
+]);
---
+++ Nop.Plugin.Widgets.mobSocial/Content/mobSocial/js/controllers/skills/skill-service.js
@@ -0,0 +1,22 @@
+﻿"use strict";
+
+app.service("SkillService", ["globalApiEndPoint", "WebClientService", function (globalApiEndPoint, WebClientService) {
+    var apiEndPoint = globalApiEndPoint + "/skills";
+    
+    this.getUserSkills = function(userId, success, error)
+    {
+        WebClientService.get(apiEndPoint + "/users/" + userId + "/get", null, success, error);
+    }
+
+    this.getSkill = function (skilId, success, error) {
+        WebClientService.get(apiEndPoint + "/get/" + skilId, null, success, error);
+    }
+
+    this.postSkill = function (skill, success, error) {
+        WebClientService.post(apiEndPoint + "/post", skill, success, error);
+    }
+
+    this.delete = function (skillId, success, error) {
+        WebClientService.delete(apiEndPoint + "/delete/" + skillId, null, success, error);
+    }
+}]);
