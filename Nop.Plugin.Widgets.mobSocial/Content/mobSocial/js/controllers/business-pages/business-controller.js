e3b3ac57105a368377f439ad62b99f7bccbcde19
anshul <anshul@apexol.com>
2017-06-19 20:12:33 +0530

    business page client pages changed according to new app architecture

---
+++ Nop.Plugin.Widgets.mobSocial/Content/mobSocial/js/controllers/business-pages/business-controller.js
@@ -0,0 +1,58 @@
+﻿app.controller("BusinessController",
+    [
+        "$scope", "businessService", function ($scope, businessService) {
+            $scope.filter = {
+                country: {},
+                stateProvince: {}
+            }
+            $scope.getById = function(id) {
+                businessService.getById(id,
+                    function(response) {
+                        if (response.Success) {
+                            $scope.business = response.ResponseData.Business;
+                        }
+                    });
+            }
+
+            $scope.getAll = function () {
+                var filter = {
+                    countryId: $scope.filter.country.Id,
+                    stateProvinceId: $scope.filter.stateProvince.Id,
+                    city: $scope.filter.city,
+                    search: $scope.filter.search,
+
+                }
+                $scope.businesses = [];
+                businessService.getAll(filter,
+                    function(response) {
+                        if (response.Success) {
+                            $scope.businesses = response.ResponseData.BusinessPages;
+                        }
+                    });
+            }
+
+            $scope.getCountries = function() {
+                businessService.getCountries(function(response) {
+                    if (response.Success) {
+                        $scope.countries = response.ResponseData.Countries;
+                    }
+                });
+            }
+
+            $scope.getStates = function(countryId) {
+                businessService.getStates(countryId, function(response) {
+                    if (response.Success) {
+                        $scope.states = response.ResponseData.States;
+                    }
+                });
+            }
+
+            $scope.initBusinessSearch = function () {
+                $scope.getCountries();
+            }
+
+            $scope.$watch('filter', function () {
+                $scope.getAll();
+            }, true);
+        }
+    ]);
---
+++ Nop.Plugin.Widgets.mobSocial/Models/BusinessPageSearchModel.cs
@@ -0,0 +1,25 @@
+﻿using Nop.Web.Framework;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Nop.Plugin.Widgets.MobSocial.Models
+{
+    public class BusinessPageSearchModel
+    {
+        public BusinessPageSearchModel()
+        {
+            AvailableStates = new List<SelectListItem>();
+            AvailableCountries = new List<SelectListItem>();
+        }
+
+        public bool StateProvinceEnabled { get; set; }
+        [NopResourceDisplayName("Address.Fields.Country")]
+        public int CountryId { get; set; }
+        [NopResourceDisplayName("Address.Fields.StateProvince")]
+        public int StateProvinceId { get; set; }
+
+        public IList<SelectListItem> AvailableStates { get; set; }
+        public IList<SelectListItem> AvailableCountries { get; set; }
+    }
+}
+
