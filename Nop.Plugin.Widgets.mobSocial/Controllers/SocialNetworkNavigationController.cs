080b8b89730a11b23a55aea2c700e49709267e97
anshul <anshul@apexol.com>
2017-06-23 17:44:49 +0530

    avoid container disposed exception for plugin controller

--- Nop.Plugin.Widgets.mobSocial/Controllers/SocialNetworkNavigationController.cs
+++ Nop.Plugin.Widgets.mobSocial/Controllers/SocialNetworkNavigationController.cs
@@ -23,13 +23,22 @@ namespace Nop.Plugin.Widgets.MobSocial.Controllers
         [ChildActionOnly]
         public ActionResult PublicInfo(string widgetZone)
         {
-            int friendRequestCount;
-            using (mobSocialEngine.ActiveEngine.IocContainer.OpenScope(Reuse.WebRequestScopeName))
+            int friendRequestCount = 0;
+            //todo: remove this call and move this to client to api call directly
+            try
             {
-                _friendService = mobSocialEngine.ActiveEngine.Resolve<IFriendService>(); //resolved from webapi standalone
+                using (mobSocialEngine.ActiveEngine.IocContainer.OpenScope(Reuse.WebRequestScopeName))
+                {
+                    _friendService =
+                        mobSocialEngine.ActiveEngine.Resolve<IFriendService>(); //resolved from webapi standalone
 
-                var currentCustomerId = _workContext.CurrentCustomer.Id;
-                friendRequestCount = _friendService.GetFriendRequests(currentCustomerId).Count;
+                    var currentCustomerId = _workContext.CurrentCustomer.Id;
+                    friendRequestCount = _friendService.GetFriendRequests(currentCustomerId).Count;
+                }
+            }
+            catch
+            {
+                //container disposed might be?
             }
             var model = new SocialNetworkNavigationModel();
 
